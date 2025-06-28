7a9f8d105fc6b72519e2ea82b133b3961e817a7d
NebulaCraft-netizen <nebulacraft101@gmail.com>
2025-06-28 09:56:45 +0500

    a

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 document.getElementById("loginBtn").addEventListener("click", () => {
   window.location.href = "/auth/google";
 });
@@ -18,3 +19,25 @@ fetch("/auth/status")
   .catch(() => {
     document.getElementById("userInfo").innerHTML = "You're not logged in.";
   });
+=======
+document.getElementById("loginBtn").addEventListener("click", () => {
+  window.location.href = "/auth/google";
+});
+
+fetch("/auth/status")
+  .then(res => res.json())
+  .then(user => {
+    if (user.name) {
+      document.getElementById("userInfo").innerHTML = `
+        <h3>👋 Logged in as ${user.name}</h3>
+        <p>Email: ${user.email}</p>
+        <img src="${user.picture}" width="60" style="border-radius: 50%;" />
+      `;
+    } else {
+      document.getElementById("userInfo").innerHTML = "You're not logged in.";
+    }
+  })
+  .catch(() => {
+    document.getElementById("userInfo").innerHTML = "You're not logged in.";
+  });
+>>>>>>> Stashed changes
